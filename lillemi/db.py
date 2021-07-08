41b4adc52cb27865ccd68665829d62c417479784
Stefan Eng <eng_steff@outlook.com>
2021-07-08 03:53:17 +0200

    Add execute and commit methods to db.py

--- lillemi/db.py
+++ lillemi/db.py
@@ -29,3 +29,12 @@ def init():
     with current_app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
+
+
+def execute(query, vals=None, fetchone=False):
+    q = get().cursor().execute(query, vals if vals else ())
+    return q.fetchone() if fetchone else q.fetchall()
+
+
+def commit():
+    get().commit()
